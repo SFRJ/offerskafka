72f0ce18892f784518b2aa0b3d8aa220e217b72f
djordje <me@mymail.com>
2020-02-28 10:16:00 +0000

    Extracting key from kafka message

--- src/main/java/com/codechallenge/offers/services/OfferManagementService.java
+++ src/main/java/com/codechallenge/offers/services/OfferManagementService.java
@@ -4,10 +4,12 @@ import com.codechallenge.offers.config.kafka.events.CreateOfferEvent;
 import com.codechallenge.offers.config.kafka.events.CreateOfferKey;
 import com.codechallenge.offers.domain.Offer;
 import com.codechallenge.offers.repositories.OffersRepository;
-import io.vavr.Value;
 import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -33,10 +35,17 @@ public class OfferManagementService {
     }
 
     @KafkaListener(topics = "${kafka.topics.creation}", groupId = "creation", containerFactory = "creationEventsListenerContainerFactory")
-    public void listenGroupCreation(CreateOfferEvent event) {
+    public void listenGroupCreation(@Payload CreateOfferEvent event, @Headers MessageHeaders headers) {
         System.out.println(String.format("Description %s", event.getDescription()));
         System.out.println(String.format("Currency %s", event.getCurrency()));
         System.out.println(String.format("Expiration %s", event.getExpiration()));
+
+        headers.keySet().forEach(key -> {
+            System.out.println(String.format("Headers %s", key));
+        });
+
+        CreateOfferKey key = (CreateOfferKey) headers.get("kafka_receivedMessageKey");
+        System.out.println(key.getOfferCreationKey());
 
         createOffer(event.getDescription(), event.getPrice(), event.getExpiration());
     }
